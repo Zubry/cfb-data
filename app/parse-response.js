32526450794fe93295518201b7cf7b333f19a4ec
James S <jamessteele@ufl.edu>
2018-11-01 16:01:32 -0400

    Write basic processing

--- app/parse-response.js
+++ app/parse-response.js
@@ -10,5 +10,31 @@ rl
   .on('line', line => processWeek(JSON.parse(line)));
 
 function processWeek(week) {
-  console.log(week);
+  week.page
+    .map(game => {
+      return {
+        home: {
+          id: game.homeTeam.id,
+          name: game.homeTeam.profileToken,
+          customName: `${game.homeTeam.location} ${game.homeTeam.name}`,
+          location: game.homeTeam.location,
+          score: game.score.homeScore,
+          scoreByPeriod: game.score.homeScoreByPeriod,
+        },
+        away: {
+          id: game.awayTeam.id,
+          name: game.awayTeam.profileToken,
+          customName: `${game.awayTeam.location} ${game.awayTeam.name}`,
+          location: game.awayTeam.location,
+          score: game.score.awayScore,
+          scoreByPeriod: game.score.awayScoreByPeriod,
+        },
+        meta: {
+          neutral: game.isNeutralSite,
+          id: game.id,
+          date: game.utcDate,
+        }
+      }
+    })
+    .map(line => (console.log(line), line))
 }
